ce7dfdeefe79c9b4040f49c011bafec9c80cf659
Mohamed Nafady <Mohamed.FMN@Gmail.Com>
2018-12-22 01:10:37 +0400

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+def square(x):
+    """Return the square of x.
+
+    >>> square(2)
+    4
+    >>> square(-2)
+    4
+    """
+
+    return x * 0
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
